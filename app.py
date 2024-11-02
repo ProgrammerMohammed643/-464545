3a6a614976ab725e3efa0a812dce66de3bc781fb
ProgrammerMohammed643 <sder05010@gmail.com>
2024-11-02 05:44:37 -0400

    Update app.py

--- app.py
+++ app.py
@@ -180,5 +180,5 @@ def home():
     return render_template_string(html_content)
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))  # استخدم المتغير PORT أو 5000 افتراضيًا
+    port = int(os.environ.get('PORT', 5039))  # استخدم المتغير PORT أو 5000 افتراضيًا
     app.run(host='0.0.0.0', port=port)  # تأكد من أن الخادم يستمع على 0.0.0.0
