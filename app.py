f0f35ab69ae835da22ad6dcaefc30eedda199f68
ProgrammerMohammed643 <sder05010@gmail.com>
2024-11-02 06:00:15 -0400

    Update app.py

--- app.py
+++ app.py
@@ -180,5 +180,5 @@ def home():
     return render_template_string(html_content)
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))  # استخدم المنفذ المحدد من قبل البيئة
+    port = int(os.environ.get("PORT", 5098))  # استخدم المنفذ المحدد من قبل البيئة
     app.run(host='0.0.0.0', port=port)  # استمع على كل الواجهات
